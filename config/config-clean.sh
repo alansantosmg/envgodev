0c7209ff46959e7446d4273fb8f4887de490c165
alansantosmg <alansantosmg@hotmail.com>
2021-02-02 12:12:00 -0300

    disabled clean swap file from config-clean.sh

--- config/config-clean.sh
+++ config/config-clean.sh
@@ -11,26 +11,26 @@ apt-get -y autoclean
 rm -rf /home/vagrant/hugo
 echo 
 
-echo "Removing swap file..."
-echo
+#echo "Removing swap file..."
+#echo
 # Remove swap file
 # does the swap file exist?
-grep -q "swapfile" /etc/fstab
+#grep -q "swapfile" /etc/fstab
 
 # if it does then remove it
-if [ $? -eq 0 ]; then
-	echo 'swapfile found. Removing swapfile.'
-	sed -i '/swapfile/d' /etc/fstab
-	echo "3" > /proc/sys/vm/drop_caches
-	swapoff -a
-	rm -f /swapfile
-else
-	echo 'No swapfile found. No changes made.'
-fi
+#if [ $? -eq 0 ]; then
+#	echo 'swapfile found. Removing swapfile.'
+#	sed -i '/swapfile/d' /etc/fstab
+#	echo "3" > /proc/sys/vm/drop_caches
+#	swapoff -a
+#	rm -f /swapfile
+#else
+#	echo 'No swapfile found. No changes made.'
+#fi
 
 # output results to terminal
-cat /proc/swaps
-cat /proc/meminfo | grep Swap
+#cat /proc/swaps
+#cat /proc/meminfo | grep Swap
 
 echo 
 echo "Setup finished."
